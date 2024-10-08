8f96bec95fc7b1caa236ee101195ceac395e3351
jungang0414 <dayr88520@gmail.com>
2024-10-08 10:50:47 +0800

    feat: Java Import&defined Packages 建立Packages，引用套件&自行建立套件

--- MyPackageClass.java
+++ MyPackageClass.java
@@ -1,7 +1,17 @@
 // 
 package mypack;
-class MyPackageClass {
+import java.util.Scanner;
+
+public class MyPackageClass {
+
+    static void txt(String name) {
+        System.out.println("Welcome Java. " + name);
+    }
+
     public static void main(String[] args) {
-        System.out.println("This is Package!");
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter your Name!");
+        String result = myObj.nextLine();
+        txt(result);
     }
 }
